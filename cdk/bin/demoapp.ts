22f2e84104c7f24fb5b51709b8b270a78b212f8b
Brett Taylor <btaylor@themeetgroup.com>
2025-04-03 16:57:12 -0400

    add use github OIDC for aws creds and security

--- cdk/bin/demoapp.ts
+++ cdk/bin/demoapp.ts
@@ -2,12 +2,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { DemoAppStack } from '../lib/demoapp-stack';
 
-const mmsandboxuse1 = { account: '123456789012', region: 'us-east-1' }
-const mmstageuse1 = { account: '123456789012', region: 'us-east-1' }
-const mmproduse1 = { account: '123456789012', region: 'us-east-1' }
+const sandboxuse2 = { account: '855363317312', region: 'us-east-2' }
+const stageuse1 = { account: '123456789012', region: 'us-east-1' }
+const produse1 = { account: '123456789012', region: 'us-east-1' }
 
 const app = new cdk.App();
 
-new DemoAppStack(app, 'DemoAppStack-sandbox-us-east-1', 'sandbox', { env: mmsandboxuse1 });
-new DemoAppStack(app, 'DemoAppStack-stage-us-east-1', 'staging', { env: mmstageuse1 });
-new DemoAppStack(app, 'DemoAppStack-prod-us-east-1', 'prod', { env: mmproduse1 });
+new DemoAppStack(app, 'DemoAppStack-sandbox-us-east-1', 'sandbox', { env: sandboxuse2 });
+new DemoAppStack(app, 'DemoAppStack-stage-us-east-1', 'staging', { env: stageuse1 });
+new DemoAppStack(app, 'DemoAppStack-prod-us-east-1', 'prod', { env: produse1 });
