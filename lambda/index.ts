bfbeeafe807215e47d5d814b172451a2bd414552
Brett Taylor <btaylor@themeetgroup.com>
2025-04-08 14:45:28 -0400

    update

--- lambda/index.ts
+++ lambda/index.ts
@@ -6,6 +6,7 @@ export const handler = async (event: HelloEvent): Promise<string> => {
   try {
     console.log(`Received event: ${JSON.stringify(event)}`);
     console.log(`Hello ${event.word}`);
+    console.log(`Hello ${event.word}`);
     return 'Success';
   } catch (error) {
     console.error(`Failed to process order: ${error instanceof Error ? error.message : 'Unknown error'}`);
